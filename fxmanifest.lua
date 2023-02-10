4eeb073d5225922beb7eb72cf161d97a4016577b
krelk-works <96574869+krelk-works@users.noreply.github.com>
2023-02-10 21:50:32 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,18 @@
+-- Resource Metadata
+fx_version 'cerulean'
+game 'gta5'
+
+author 'krelk <krelk#2970>'
+description 'Street Notifier Script'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/css/styles.css',
+    'html/js/main.js'
+}
+
+client_scripts {'cl/*.lua'}
+shared_scripts {'shared/*.lua'}
